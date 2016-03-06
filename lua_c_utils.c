ed4fa908b09617e9120807cf178dfb62cdf81109
hackerwin7 <1186897618@qq.com>
2016-03-06 18:34:26 +0800

    rename lua_utils.c to lua_c_utils.c

--- lua_c_utils.c
+++ lua_c_utils.c
@@ -1,6 +1,6 @@
 //
 // Created by fff on 3/5/16.
-//
+// C for lua call use only
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -10,3 +10,12 @@
 #include "gzip-lib/gzip_util.h"
 #include "base64-lib/base64_util.h"
 
+int init() {
+    /*no op*/
+    return 0;
+}
+
+int close() {
+    /*no op*/
+    return 0;
+}
